cc73be144591470f9acb2aa8de424406d40052bd
Oliver <oliver.elfverson@outlook.com>
2026-08-27 17:47:27 +0200

    WOrks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
-    import ModeToggle from "$lib/components/ModeToggle.svelte";
+import ModeToggle from "$lib/components/ModeToggle.svelte";
+
+let input_content = $state("");
+
+function play_sound() {
+  const sound = new Audio("1.wav");
+  sound.play();
+}
+
+function handle_submit(event: SubmitEvent) {
+  event.preventDefault();
+  play_sound();
+  input_content = "";
+}
 
 </script>
 
@@ -7,7 +20,13 @@
   <nav class="w-full flex justify-end p-4">
     <ModeToggle />
   </nav>
-  <main>
-    <input type="text" class=""/>
+  <main class="flex size-full justify-center items-center">
+    <form onsubmit={handle_submit} class="min-w-32 max-w-lg h-16 w-[50%] ">
+      <input
+        type="text"
+        class="bg-foreground outline-4 outline-accent text-accent size-full font-bold p-2 text-4xl dark:text-background"
+        bind:value={input_content}
+      />
+    </form>
   </main>
 </div>
